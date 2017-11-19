dd0c282631525ff38e6fb0ae49e8b2882020ae67
DESKTOP-E89609C\Chinnie <chinnawat13577@gmail.com>
2017-11-19 21:05:30 +0700

    Add RestfulUtility.cs Add login btn (morkup )

---
+++ Assets/Colosseum/Scrpits/LoginPage/FacebookLoginButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacebookLoginButton : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public void OnClick()
+    {
+        WWWForm post = new WWWForm();
+        post.AddField("id", "chinnie");
+        post.AddField("password", 69);
+        StartCoroutine(RestfulUtility.Post("127.0.0.1:3000", post, (data) =>
+        {
+            print(data.text);
+        }));
+    }
+}
---
+++ Assets/Colosseum/Scrpits/RestfulUtility.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class RestfulUtility
+{
+    /// <summary>
+    /// Function PUT in REST API.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="data">raw data</param>
+    /// <param name="callback">return callback from backend.</param>
+    /// <returns></returns>
+    public static IEnumerator Put(string url, byte[] data, Action<DownloadHandler> callback = null)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Put(url, data))
+        {
+            yield return request.Send();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log(request.responseCode);
+                if (callback != null)
+                    callback(request.downloadHandler);
+            }
+        }
+    }
+    /// <summary>
+    /// Function "PUT" in REST API.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="data">raw data</param>
+    /// <param name="callback">return callback from backend.</param>
+    /// <returns></returns>
+    public static IEnumerator Put(string url, string data, Action<DownloadHandler> callback = null)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Put(url, data))
+        {
+            yield return request.Send();
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log(request.responseCode);
+                if (callback != null)
+                    callback(request.downloadHandler);
+            }
+        }
+    }
+    /// <summary>
+    /// Function GET in REST API.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="callback">return callback from backend.</param>
+    /// <returns></returns>
+    public static IEnumerator Get(string url, Action<DownloadHandler> callback = null)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.Send();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log(request.responseCode);
+            if (callback != null)
+                callback(request.downloadHandler);
+        }
+    }
+    /// <summary>
+    /// Function POST in REST API.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="form">Unity WWWForm</param>
+    /// <param name="callback">return callback from backend.</param>
+    /// <returns></returns>
+    public static IEnumerator Post(string url, WWWForm form, Action<DownloadHandler> callback = null)
+    {
+        UnityWebRequest request = UnityWebRequest.Post(url, form);
+        yield return request.Send();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log(request.responseCode);
+            if (callback != null)
+                callback(request.downloadHandler);
+        }
+    }
+    /// <summary>
+    /// return Texture from url.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="callback">return Unity Texture.</param>
+    /// <returns></returns>
+    public static IEnumerator GetImage(string url, Action<Texture> callback)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        yield return request.Send();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log(request.responseCode);
+            if (callback != null)
+                callback(((DownloadHandlerTexture)request.downloadHandler).texture);
+        }
+    }
+}
