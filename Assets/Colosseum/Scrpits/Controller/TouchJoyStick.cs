fe745444757be89aaddc651b62c9df9547b98541
DESKTOP-E89609C\Chinnie <chinnawat13577@gmail.com>
2017-12-03 20:19:15 +0700

    Config facebook api. Add socket.io to node js

---
+++ Assets/Colosseum/Scrpits/Controller/TouchJoyStick.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TouchJoyStick : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndDragHandler
+{
+
+    public RectTransform button;
+    RectTransform rect;
+    Vector2 defaultPos;
+
+    void Awake()
+    {
+        Initial();
+    }
+    void Initial()
+    {
+        rect = GetComponent<RectTransform>();
+        print(defaultPos);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, eventData.position, eventData.enterEventCamera, out pos);
+        pos.x = (pos.x / rect.sizeDelta.x) * 2 - 1;
+        pos.y = (pos.y / rect.sizeDelta.y) * 2 + 1;
+        pos = (pos.magnitude > 1) ? pos.normalized : pos;
+        button.anchoredPosition = new Vector2(pos.x * rect.sizeDelta.x / 2.5f, pos.y * rect.sizeDelta.y / 2.5f);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, eventData.position, eventData.enterEventCamera, out pos);
+        pos.x = (pos.x / rect.sizeDelta.x) * 2 - 1;
+        pos.y = (pos.y / rect.sizeDelta.y) * 2 + 1;
+        pos = (pos.magnitude > 1) ? pos.normalized : pos;
+        button.anchoredPosition = new Vector2(pos.x * rect.sizeDelta.x / 2.5f, pos.y * rect.sizeDelta.y / 2.5f);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        button.anchoredPosition = Vector2.zero;
+    }
+
+}
